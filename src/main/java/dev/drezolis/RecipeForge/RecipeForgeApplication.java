bb35cbc5d1c379f39ccc280d988ce211bdec028c
Andre Gonçalves <dregoncalves22@gmail.com>
2026-03-17 11:22:44 -0300

    Initial commit

---
+++ src/main/java/dev/drezolis/RecipeForge/RecipeForgeApplication.java
@@ -0,0 +1,13 @@
+package dev.drezolis.RecipeForge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeForgeApplication {
+
+	public static void main(String[] args) {
+			SpringApplication.run(RecipeForgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/drezolis/RecipeForge/RecipeForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.drezolis.RecipeForge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

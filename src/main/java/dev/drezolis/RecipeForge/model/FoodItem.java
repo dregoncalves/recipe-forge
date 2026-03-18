1103e8cb25998216adeb39ec3ad1f5aed56100ca
Andre Gonçalves <dregoncalves22@gmail.com>
2026-03-18 12:57:35 -0300

    feat: add Lombok @Data and validation to FoodItem

--- src/main/java/dev/drezolis/RecipeForge/model/FoodItem.java
+++ src/main/java/dev/drezolis/RecipeForge/model/FoodItem.java
@@ -1,7 +1,11 @@
 package dev.drezolis.RecipeForge.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
@@ -10,57 +14,23 @@ import java.time.LocalDate;
 @Table(name = "food_item")
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class FoodItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "O alimento não pode existir sem um nome.")
     private String name;
 
+    @Enumerated(EnumType.STRING)
     private Category category;
 
+    @NotNull(message = "A quantidade do alimento não pode ser nula.")
+    @Min(1)
     private Integer quantity;
 
     private LocalDate expiryDate;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public LocalDate getExpiryDate() {
-        return expiryDate;
-    }
-
-    public void setExpiryDate(LocalDate expiryDate) {
-        this.expiryDate = expiryDate;
-    }
 }
